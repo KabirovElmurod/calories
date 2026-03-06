d3eca3b6bfc139b491dd92399ce5f310da9a9f41
Meylis <meylismeretdurdiyev0@gmail.com>
2026-03-06 15:18:01 +0500

    Home page changed

--- Dizayn/script.js
+++
@@ -1,8 +0,0 @@
-function openInfo(text) {
-  document.getElementById("modal-text").innerText = text;
-  document.getElementById("modal").style.display = "block";
-}
-
-function closeModal() {
-  document.getElementById("modal").style.display = "none";
-}
